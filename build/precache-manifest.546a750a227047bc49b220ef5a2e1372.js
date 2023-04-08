aa52f393bd52dd27a86e888b76e2cc7d50403a36
Sergiy Ivanov <sg.antiiva@gmail.com>
2023-04-08 17:38:58 +0300

    before deployment

---
+++ build/precache-manifest.546a750a227047bc49b220ef5a2e1372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10359f98bddccc58dca",
+    "url": "/static/css/main.c38914fb.chunk.css"
+  },
+  {
+    "revision": "f10359f98bddccc58dca",
+    "url": "/static/js/main.f10359f9.chunk.js"
+  },
+  {
+    "revision": "3bc3a3f17d78ae947b63",
+    "url": "/static/css/1.cc5c313b.chunk.css"
+  },
+  {
+    "revision": "3bc3a3f17d78ae947b63",
+    "url": "/static/js/1.3bc3a3f1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eae1d8b4e38d2606fa56b2f13134adbd",
+    "url": "/index.html"
+  }
+];
